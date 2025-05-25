441e046d31f649fe02fbc4285f5c5c5280e362ab
Nonotch59 <arnaud.seron@gmail.com>
2025-05-25 11:14:11 +0200

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,5 @@
 // ✅ Initialisation Supabase avec tes vraies infos
-
-const supabaseUrl = 'https://bmanspqauleydsxdvtpy.supabase.co';/ ✅ Corrigé ici
+const supabaseUrl = 'https://bmanspqauleydsxdvtpy.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJtYW5zcHFhdWxleWRzeGR2dHB5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMwNjgwNzEsImV4cCI6MjA1ODY0NDA3MX0.-m80mGof0FL3pFU0m1_UOGcNVpo9xm1WD037ZtFAy-w';
 
 const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
