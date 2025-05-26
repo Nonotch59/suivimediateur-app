28ebf3be0c009b619df4db717764c2b5393958ef
Nonotch59 <arnaud.seron@gmail.com>
2025-05-26 17:36:04 +0200

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,11 +1,11 @@
 // ✅ Initialisation Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyDf8hbsFHQ5MAEssfM6SeF2s1Glc0JTS7U",
+  apiKey: "AIzaSyDFuy1uG_BFe_Grwivxm-32fQgX2zrgsjo",
   authDomain: "suivimediateuras.firebaseapp.com",
   projectId: "suivimediateuras",
   storageBucket: "suivimediateuras.appspot.com",
   messagingSenderId: "385344535314",
-  appId: "1:385344535314:web:019fb10e56e4a408a491cb"
+  appId: "1:385344535314:web:d5f12ee231ac55aca491cb"
 };
 
 firebase.initializeApp(firebaseConfig);
