7c21144a5dccaf1241b97c77a7c9fe419ea9e940
Nonotch59 <arnaud.seron@gmail.com>
2025-05-29 17:50:30 +0200

    Delete js directory

--- js/supabase.js
+++
@@ -1,6 +0,0 @@
-// ✅ Initialisation Supabase propre
-const supabaseUrl = 'https://bmanspqauleydsxdvtpy.supabase.co';
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJtYW5zcHFhdWxleWRzeGR2dHB5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMwNjgwNzEsImV4cCI6MjA1ODY0NDA3MX0.-m80mGof0FL3pFU0m1_UOGcNVpo9xm1WD037ZtFAy-w";
-
-const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
-console.log("✅ [Supabase] Client initialisé");
