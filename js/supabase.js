310c986d507265a4cf767577c4bf766fbcdd9b25
Nonotch59 <arnaud.seron@gmail.com>
2025-05-29 17:58:52 +0200

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,5 @@
+const supabase = window.supabase.createClient(
+  "https://bmanspqauleydsxdvtpy.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJtYW5zcHFhdWxleWRzeGR2dHB5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMwNjgwNzEsImV4cCI6MjA1ODY0NDA3MX0.-m80mGof0FL3pFU0m1_UOGcNVpo9xm1WD037ZtFAy-w"
+);
+console.log("✅ [Supabase] Initialisé");
